d9012b782834d5dc28d2c5e90d84eb252317954e
rabbit <ttbowers21@gmail.com>
2021-05-24 15:28:44 -0700

    Simple Redux additions

--- src/index.js
+++ src/index.js
@@ -3,6 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+// STORE -> Globalized state(s)
+
+// ACTION -> change state 'how many todo items'
+const count_todo = () => {
+  return {
+    type: 'ADD';
+  }
+}
+
+const rm_todo = () => {
+  return {
+    type: 'SUB'
+  }
+}
+
+// REDUCER -> how actions transform state to next
+const curr_total = (state = 0, action) => {
+  switch(action.type){
+    case "ADD":
+      return state + 1;
+    case "SUB":
+      return state -1;
+  }
+}
+
+let store = createStore(curr_total);
+
+// Display items in amount in console
+store.subscribe(() => console.log(store.getState());
+
+// DISPATCH -> runs action
+store.dispatch(count_todo());
 
 ReactDOM.render(
   <React.StrictMode>
